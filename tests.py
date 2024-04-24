4e4f6d8b50bf1d04ecea8f3e6ca09829eee4c0e2
qus0in <qus0in@gmail.com>
2024-04-24 14:31:11 +0900

    var3

--- tests.py
+++ tests.py
@@ -3,16 +3,16 @@ import os
 
 class Tests(unittest.TestCase):
     def test_one(self):
-        var1 = -1
+        var1 = 1
         self.assertEqual(var1, 1, 'please set var1 to 1')
 
     def test_two(self):
         var2 = int(os.getenv('var2'))
         self.assertEqual(var2, 2, 'please set var2 to 2')
     
-    def test_three(self):
-        var3 = os.getenv('var3')
-        self.assertEqual(var3, 3, 'please set var3 to 3')
-
+    def test_two(self):
+        var2 = int(os.getenv('var3'))
+        self.assertEqual(var2, 2, 'please set var2 to 2')
+    
 if __name__ == '__main__':
     unittest.main()
