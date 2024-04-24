d724fb12639664d71ea84c989b06854126f75301
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2024-04-24 14:32:45 +0900

    three

--- tests.py
+++ tests.py
@@ -10,9 +10,9 @@ class Tests(unittest.TestCase):
         var2 = int(os.getenv('var2'))
         self.assertEqual(var2, 2, 'please set var2 to 2')
     
-    def test_two(self):
-        var2 = int(os.getenv('var3'))
-        self.assertEqual(var2, 2, 'please set var2 to 2')
+    def test_three(self):
+        var3 = int(os.getenv('var3'))
+        self.assertEqual(var3, 2, 'please set var3 to 3')
     
 if __name__ == '__main__':
     unittest.main()
